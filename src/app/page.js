621509a6c13c6debad70b911f1ae3d0dc057b1ff
KranthiBhai <kk232243@gmail.com>
2024-01-07 17:05:43 +0530

    added loading effect in main page file

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import Shell from '@/components/shells/Shell'
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Shell from "@/components/shells/Shell";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
@@ -10,9 +10,12 @@ export default function Home() {
   useEffect(() => {
     router.push('/shopping/orders');
   }, []);
+
   return (
-   <div className=' w-full h-full'>
-    <Shell />
-   </div>
-  )
+    <div className=" w-full h-full flex items-center justify-center bg-clrbg">
+      <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black py-1 italic ">
+        Loading...
+      </h1>
+    </div>
+  );
 }
